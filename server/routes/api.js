101972af2b4a46596a58d86e5c93d7d1d164ba04
Nishant2604 <Nishant@DESKTOP-UV63V73.localdomain>
2018-04-24 20:29:58 +0530

    Pushed on April 24, 2018

---
+++ server/routes/api.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+// Connect
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://localhost:27017/mean', (err, client) => {
+        if (err) return console.log(err);
+
+        let db = client.db('mean');
+        closure(db);
+    });
+};
+
+// Error handling
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+// Get users
+router.get('/users', (req, res) => {
+    connection((db) => {
+        db.collection('users')
+            .find()
+            .toArray()
+            .then((users) => {
+                response.data = users;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+
+module.exports = router;
