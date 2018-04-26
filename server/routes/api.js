7a126578613ea7e24f299124246889f7e5b23469
Nishant <nishantnischaya@gmail.com>
2018-04-26 12:32:16 +0530

    Update April 26, 2018

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,10 +5,10 @@ const ObjectID = require('mongodb').ObjectID;
 
 // Connect
 const connection = (closure) => {
-    return MongoClient.connect('mongodb://localhost:27017/mean', (err, client) => {
+    return MongoClient.connect('mongodb://localhost:27017/users', (err, client) => {
         if (err) return console.log(err);
 
-        let db = client.db('mean');
+        let db = client.db('users');
         closure(db);
     });
 };
