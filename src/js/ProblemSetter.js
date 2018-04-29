b9a7e9a047e039ddb1da3385a76e26fcd888c917
Nishant <nishantnischaya@gmail.com>
2018-04-29 18:45:59 +0530

    Updated April, 29, 2018

---
+++ src/js/ProblemSetter.js
@@ -0,0 +1,18 @@
+let users = []
+
+$.get('/api/problems', (object) => {
+    obj = {
+        problems: object.data,
+    }
+
+    createHTML(obj);
+})
+
+function createHTML(problems) {
+    var rawTemplate = document.getElementById("problem_template").innerHTML;
+    var compiledTemplate = Handlebars.compile(rawTemplate);
+    var GeneratedHTML = compiledTemplate(problems);
+
+    var container = document.getElementById("problem_container");
+    container.innerHTML = GeneratedHTML;
+}
